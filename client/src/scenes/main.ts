62a1af596c20a2014af4aaa47e0efa3b83e10cf6
Niku <gronbergniku@gmail.com>
2022-01-09 18:41:43 +0200

    the square is moving

--- client/src/scenes/main.ts
+++ client/src/scenes/main.ts
@@ -5,16 +5,40 @@ const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
 };
 
 export class GameScene extends Phaser.Scene {
-  private square?: Phaser.GameObjects.Rectangle;
+  private square?: Phaser.GameObjects.Rectangle & {
+    body: Phaser.Physics.Arcade.Body;
+  };
 
   constructor() {
     super(sceneConfig);
+    this.square = undefined;
   }
 
   public create() {
-    this.square = this.add.rectangle(400, 400, 100, 100, 0x00ffff);
-    this.physics.add.existing(this.square);
+    this.square = this.add.rectangle(400, 400, 100, 100, 0x00ffff) as any;
+
+    if (this.square) {
+      this.physics.add.existing(this.square);
+    }
   }
 
-  public update() {}
+  public update() {
+    const cursorKeys = this.input.keyboard.createCursorKeys();
+
+    if (cursorKeys.up.isDown) {
+      this.square?.body.setVelocityY(-500);
+    } else if (cursorKeys.down.isDown) {
+      this.square?.body.setVelocityY(500);
+    } else {
+      this.square?.body.setVelocityY(0);
+    }
+
+    if (cursorKeys.left.isDown) {
+      this.square?.body.setVelocityX(-500);
+    } else if (cursorKeys.right.isDown) {
+      this.square?.body.setVelocityX(500);
+    } else {
+      this.square?.body.setVelocityX(0);
+    }
+  }
 }
