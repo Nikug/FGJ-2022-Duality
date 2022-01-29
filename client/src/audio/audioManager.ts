cab3de07c9f9619c867323522a51a272ede239e6
Niku <gronbergniku@gmail.com>
2022-01-30 00:45:30 +0200

    add main music

--- client/src/audio/audioManager.ts
+++ client/src/audio/audioManager.ts
@@ -2,6 +2,7 @@ const dashWav = "/assets/audio/dash.wav";
 const jumpWav = "/assets/audio/jump.wav";
 const collectWav = "/assets/audio/collect.wav";
 const smackWav = "/assets/audio/smack.wav";
+const musicMp3 = "/assets/audio/FGJ 2022 Duality.mp3";
 
 /* Creates Audio Manager to use in scene
   Initialize AudioManager in scene create()
@@ -23,6 +24,7 @@ export class AudioManager {
     this.scene.load.audio("collect", collectWav);
     this.scene.load.audio("jump", jumpWav);
     this.scene.load.audio("smack", smackWav);
+    this.scene.load.audio("music", musicMp3);
   }
 
   addAudio() {
@@ -30,6 +32,7 @@ export class AudioManager {
     const collect = this.scene.sound.add("collect", { volume: 0.25 });
     const jump = this.scene.sound.add("jump");
     const smack = this.scene.sound.add("smack");
+    const music = this.scene.sound.add("music", { volume: 0.25, loop: true });
 
     this.scene.events.on("playCollect", () => {
       collect.play();
@@ -45,6 +48,9 @@ export class AudioManager {
 
     this.scene.events.on("playSmack", () => {
       smack.play();
+    });
+    this.scene.events.on("create", () => {
+      music.play();
     });
   }
 }
